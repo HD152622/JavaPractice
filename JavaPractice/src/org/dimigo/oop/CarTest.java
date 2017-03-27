7a33ea8ecb585b6f777b033f0b785f5fded79c55
tracer12 <vendetta12@naver.com>
2017-03-27 12:29:09 +0900

    asdf

---
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_CarTest
+ *
+ * 1.개요:
+ * 2.작성일: 2017. 3. 27.
+ * </pre>
+ *
+ * @author    : 유민
+ * @version   : 1.0
+ */
+public class CarTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
