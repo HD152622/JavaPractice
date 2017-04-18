93b0a79e5bb58b224195f0c118c8b21d9591eaf9
tracer12 <vendetta12@naver.com>
2017-04-18 13:49:41 +0900

    돼지저금통

---
+++ JavaPractice/src/org/dimigo/oop/FamilyMember.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_FamilyMembers
+ *
+ * 1.개요:
+ * 2.작성일: 2017. 4. 18.
+ * </pre>
+ *
+ * @author    : 유민
+ * @version   : 1.0
+ */
+public class FamilyMember {
+
+	private static int memberCnt;
+	
+	private String memberName;
+	
+	public FamilyMember (String memberName){
+		this.memberName = memberName;
+		memberCnt++;
+	}
+	
+	public String getMemberName(){
+		return memberName;
+	}
+	
+	public static void printMemberCnt(){
+		System.out.println("가족인원수 : "+memberCnt + "명");
+	}
+}
